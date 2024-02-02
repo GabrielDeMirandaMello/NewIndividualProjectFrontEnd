81a2a212e5d11f13e100eee41714a4ae60972749
Gabriel Mello <gabriel.mmello@sptech.school>
2024-02-01 22:12:14 -0300

    navbar pronto

--- src/Pages/Home/navbar/Navbar.jsx
+++ src/Pages/Home/navbar/Navbar.jsx
@@ -8,7 +8,10 @@ function NavBar() {
     return (
         <>
             <div className='content-navbar'>
+                <div>
                 <img className='details-img' src={imgLogo} alt="imagem de um planeta com um avião ao lado" />
+                <span>History Travels</span>
+                </div>
                 <div className='buttons-menu'>
                     <button className='button-cadastro'>Cadastro</button>
                     <button className='button-entrar'>Entrar</button>
